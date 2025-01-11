fac1aad605dc5ed51ec0c5e98b4745ab296cf866
Marin Mema <marinmema@live.ca>
2025-01-11 12:58:00 -0500

    delete context menu

--- background.js
+++ background.js
@@ -1,23 +1,6 @@
 // The Project Gutenberg eBook of Frankenstein; Or, The Modern Prometheus
 // https://www.gutenberg.org/cache/epub/84/pg84-images.html
 
-chrome.runtime.onInstalled.addListener(createContextMenus);
-chrome.contextMenus.onClicked.addListener(onContextMenuClick);
-
-function createContextMenus() {
-	chrome.contextMenus.create({
-		id: '1',
-		title: 'Frankenstein',
-		contexts: ['editable'],
-	});
-}
-
-function onContextMenuClick(info) {
-	if (info.menuItemId == '1') {
-		console.log(info);
-	}
-}
-
 chrome.runtime.onMessage.addListener(onMessage);
 
 function onMessage(request, sender, sendResponse) {
