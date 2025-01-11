122b7835f21fcc3f25bf63e0680a2c5e4609f266
Marin Mema <marinmema@live.ca>
2025-01-11 12:56:07 -0500

    get match from background script

--- background.js
+++ background.js
@@ -17,3 +17,24 @@ function onContextMenuClick(info) {
 		console.log(info);
 	}
 }
+
+chrome.runtime.onMessage.addListener(onMessage);
+
+function onMessage(request, sender, sendResponse) {
+	sendResponse(REPLACEMENTS.find(r => request.string.endsWith(r.from)));
+}
+
+const REPLACEMENTS = [
+	{
+		'from': "mary1;",
+		'to': "You will rejoice to hear that no disaster has accompanied the commencement of an enterprise which you have regarded with such evil forebodings."
+	},
+	{
+		'from': "mary2;",
+		'to': "How slowly the time passes here, encompassed as I am by frost and snow!"
+	},
+	{
+		'from': "mary3;",
+		'to': "I write a few lines in haste to say that I am safe—and well advanced on my voyage.",
+	}
+];
--- page.js
+++ page.js
@@ -8,27 +8,12 @@ document.querySelectorAll(targets).forEach(el => {
 	});
 });
 
-function insertInto(target) {
-	target.value = replacement(String(target.value)) ?? target.value;
+async function insertInto(target) {
+	target.value = await replacement(String(target.value)) ?? target.value;
 }
 
-const REPLACEMENTS = [
-	{
-		'from': "mary1;",
-		'to': "You will rejoice to hear that no disaster has accompanied the commencement of an enterprise which you have regarded with such evil forebodings."
-	},
-	{
-		'from': "mary2;",
-		'to': "How slowly the time passes here, encompassed as I am by frost and snow!"
-	},
-	{
-		'from': "mary3;",
-		'to': "I write a few lines in haste to say that I am safe—and well advanced on my voyage.",
-	}
-];
-
-function replacement(string) {
-	const match = REPLACEMENTS.find(r => string.endsWith(r.from));
+async function replacement(string) {
+	const match = await chrome.runtime.sendMessage({ 'string': string.slice(-20) });
 
 	return match
 		? string.substring(0, -match.from.length) + match.to
